e12abef3c75fcd8d514a49eefa7d631b88a9ac96
Maksim Shaidulin <mshaydulin@gmail.com>
2020-05-04 20:18:38 +0200

    fix encoding

--- tbot.py
+++ tbot.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(token)
 @bot.message_handler()
 def hangle_message(message):
     print(message.text)
-    bot.send_message(message.chat.id, f'ты сказал {message.text}')
+    bot.send_message(message.chat.id, f'You said: {message.text}')
     # bot.send_location(message.chat.id, 55,55)
 
 bot.polling()
